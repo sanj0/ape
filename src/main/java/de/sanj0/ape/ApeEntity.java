2d04680f4bc0e78ae1c1a5a3eaa09c19714efe1d
Malte <malte.dostal@gmail.com>
2020-12-25 21:57:33 +0100

    add empty ApeEntity interface

---
+++ src/main/java/de/sanj0/ape/ApeEntity.java
@@ -0,0 +1,13 @@
+package de.sanj0.ape;
+
+/**
+ * An ApeEntity is an entity in the
+ * simulated world. This interface
+ * specifies basic functionality
+ * every entity in said world has
+ * to offer to properly interact
+ * with the other entities and the
+ * world itself.
+ */
+public interface ApeEntity {
+}
